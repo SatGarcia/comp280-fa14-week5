552d1c80ec8bf97d06c42b043f8211412c05bf01
Sat Garcia <elsaturnino@gmail.com>
2014-10-02 10:15:08 -0700

    filled in main.c

---
+++ obj/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "mystery.h"
+
+int main() {
+	printf("Time to start the mysterious code!\n");
+
+	start_mystery();
+
+	printf("The mystery has ended.\n");
+
+	return 0;
+}
